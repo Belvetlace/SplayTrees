935b1028b4ed4dd86a05b7dad6e0cffd07c34a72
dmytromarych <marich96@mail.ru>
2018-05-12 18:48:16 -0700

    starts on insert() and remove(), splay() in progress

--- srs/FHsplayTree.java
+++ srs/FHsplayTree.java
@@ -7,15 +7,67 @@ public class FHsplayTree<E extends Comparable<? super E>>
    @Override
    public boolean insert( E x )
    {
+      int compareResult;
+      FHs_treeNode mRoot = super.mRoot;
 
+      if (mRoot == null)
+      {
+         mRoot = new FHs_treeNode(x,null,null);
+         mSize++;
+         return true;
+      }
+
+      FHs_treeNode newNode, parent;
+      parent = mRoot;
+
+      while(true)
+      {
+         compareResult = parent.data.compareTo(x);
+         if ( compareResult > 0 )
+         {
+            if( parent.lftThread != null )
+               parent = parent.lftChild;
+            else
+            {
+               // place as new left child
+               newNode = new FHs_treeNode(x, parent.lftChild, parent);
+               parent.lftChild = newNode;
+               parent.lftThread = false;
+               break;
+            }
+         }
+         else if ( compareResult < 0 )
+         {
+            if( !(parent.rtThread) )
+               parent = parent.rtChild;
+            else
+            {
+               // place as new right child
+               newNode = new FHs_treeNode
+                       (x, parent, parent.rtChild);
+               parent.rtChild = newNode;
+               parent.rtThread = false;
+               break;
+            }
+         }
+         else
+            return false;  // duplicate
+      }
+
+      mSize++;
       return true;
    }
 
    @Override
    public boolean remove( E x )
    {
-
-      return true;
+      FHs_treeNode mRoot = super.mRoot;
+      if(mRoot == null){
+         return false;
+      }
+      else{
+         splay(super.mRoot, x);
+      }
    }
 
    // returns (doesn't really show) the m_root data,
@@ -34,8 +86,42 @@ public class FHsplayTree<E extends Comparable<? super E>>
    // and a detailed algorithm is given there.
    protected FHs_treeNode<E> splay( FHs_treeNode<E> root, E x )
    {
+      FHs_treeNode<E> rightTree, leftTree, rightTreeMin, leftTreeMax = null;
+      while(root != null){
+         int compareResult = root.data.compareTo(x);
+         if(compareResult > 0) {
+            if (root.lftChild == null) {
+               break;
+            }
+            compareResult = root.lftChild.data.compareTo(x);
+            if (compareResult > 0) {
+               FHs_treeNode foo = rotateWithLeftChild(root);
+               if (foo.lftChild == null) {
+                  break;
+               }
+               //add root to rightTree under rightTree's minimum node (as the left child of rightTreeMin), unless of course rightTree is null (in which case you should set rightTree to be root)
+               //then update the rightTreeMin to point to the working root node
 
-      return null;
+            }
+         }
+            //update the new working root:  set root to root's left child
+         if(compareResult < 0){
+               if(root.rtChild == null){
+                  break;
+               }
+               compareResult = root.rtChild.data.compareTo(x);
+               if(compareResult < 0){
+                  FHs_treeNode foo = rotateWithRightChild(root);
+                  if(foo.rtChild == null){
+                     break;
+                  }
+               }
+               //add root to leftTree under leftTree's maximum node (as the left child of leftTreeMax), unless of course leftTree is null (in which case you should set leftTree to be root)
+              // then update the leftTreeMax to point to the working root node
+              // update the new working root:  set root to root's right child
+            }
+            break;
+      }
    }
 
 
